61b78e14464d098c7465d8e6884bd7b581e094e1
DT3264 <cernadaniel@gmail.com>
2019-12-31 08:28:24 -0600

    Added carga & kardex. All done.

--- lib/Pages/carga_page.dart
+++ lib/Pages/carga_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:sicedroid/Models/carga_academica.dart';
+import 'package:sicedroid/Models/materia_carga_academica.dart';
 import 'package:sicedroid/Routes/routes.dart';
 import 'package:sicedroid/Utils/singleton.dart';
+import 'package:sicedroid/Widgets/back_button_handler.dart';
 import 'package:sicedroid/Widgets/infinite_loading.dart';
 import 'package:sicedroid/Widgets/main_drawer.dart';
 
@@ -14,34 +16,136 @@ class CargaPage extends StatefulWidget {
 class _CargaPageState extends State<CargaPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Carga Académica'),
-      ),
-      drawer: MainDrawer(
-        context: context,
-        page: Routes.carga,
-      ),
-      body: FutureBuilder(
-        future: Singleton.get().webServiceAlumnos.getCargaAcademica(),
-        builder: (buildContext, snapshot) {
-          if (snapshot.connectionState != ConnectionState.done) {
-            return InfiniteLoading();
-          } else if (!snapshot.hasData) {
-            return Center(child: Text('Conexión fallida, intnete más tarde'));
-          }
-          return _getBody(snapshot.data);
-        },
+    return DefaultTabController(
+      length: 7,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Carga Académica'),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: <Widget>[
+              Tab(text: 'HOY'),
+              Tab(text: 'LUN'),
+              Tab(text: 'MAR'),
+              Tab(text: 'MIE'),
+              Tab(text: 'JUE'),
+              Tab(text: 'VIE'),
+              Tab(text: 'SAB')
+            ],
+          ),
+        ),
+        drawer: MainDrawer(
+          context: context,
+          page: Routes.carga,
+        ),
+        body: BackButtonHandler(
+          context,
+          child: FutureBuilder(
+            future: Singleton.get().webServiceAlumnos.getCargaAcademica(),
+            builder: (buildContext, snapshot) {
+              if (snapshot.connectionState != ConnectionState.done) {
+                return InfiniteLoading();
+              } else if (!snapshot.hasData) {
+                return Center(
+                    child: Text('Conexión fallida, intnete más tarde'));
+              }
+              return TabBarView(
+                children: <Widget>[
+                  getHoy(carga: snapshot.data),
+                  getSingleDay(dia: 1, carga: snapshot.data, showDate: false),
+                  getSingleDay(dia: 2, carga: snapshot.data, showDate: false),
+                  getSingleDay(dia: 3, carga: snapshot.data, showDate: false),
+                  getSingleDay(dia: 4, carga: snapshot.data, showDate: false),
+                  getSingleDay(dia: 5, carga: snapshot.data, showDate: false),
+                  getSingleDay(dia: 6, carga: snapshot.data, showDate: false)
+                ],
+              );
+            },
+          ),
+        ),
       ),
     );
   }
 
-  Widget _getBody(CargaAcademica carga) {
+  Widget getHoy({@required CargaAcademica carga}) {
+    var dia = DateTime.now().weekday;
+    if (dia != 7) {
+      return getSingleDay(dia: dia, carga: carga, showDate: true);
+    } else {
+      return Center(
+        child: Text('Sin horario el domingo'),
+      );
+    }
+  }
+
+  Widget getSingleDay(
+      {@required int dia,
+      @required CargaAcademica carga,
+      @required bool showDate}) {
+    var hoy = DateTime.now();
+    List<MateriaCargaAcademica> materias =
+        carga.getMateriasDelDia(getDiaDeInt(dia));
+    List<Widget> materiasWidgets = [];
+    materiasWidgets.add(Container(
+      color: Colors.green,
+      width: MediaQuery.of(context).size.width * .8,
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.only(bottom: 5),
+      child: Center(
+          child: Text(
+        '${getDiaDeInt(dia)}' +
+            (showDate ? ': ${hoy.day}/${hoy.month}/${hoy.year}' : ''),
+        style: TextStyle(color: Colors.white),
+      )),
+    ));
+    for (var i = 0; i < materias.length; i++) {
+      materiasWidgets.add(getMateriaRow(materias[i], i == materias.length - 1));
+    }
     return SingleChildScrollView(
-      child: Container(
-        color: Colors.green,
-        child: Text(carga.materias.toString()),
+      child: Padding(
+        padding: EdgeInsets.only(top: 10, bottom: 10),
+        child: Column(children: materiasWidgets),
       ),
     );
+  }
+
+  Widget getMateriaRow(MateriaCargaAcademica m, bool isLast) {
+    var color = Colors.yellow[700];
+    return Container(
+        width: MediaQuery.of(context).size.width * .8,
+        padding: EdgeInsets.all(10),
+        decoration: ShapeDecoration(
+            shape: BorderDirectional(
+          top: BorderSide(color: color),
+          bottom: isLast ? BorderSide(color: color) : BorderSide.none,
+          start: BorderSide(color: color),
+          end: BorderSide(color: color),
+        )),
+        child: Column(
+          children: <Widget>[
+            Text(m.materia),
+            Text(m.docente),
+            Text(m.horaDiaActual),
+            Text('Aula: ${m.aulaDiaActual}')
+          ],
+        ));
+  }
+
+  String getDiaDeInt(int dia) {
+    if (dia == 1) {
+      return 'Lunes';
+    } else if (dia == 2) {
+      return 'Martes';
+    } else if (dia == 3) {
+      return 'Miercoles';
+    } else if (dia == 4) {
+      return 'Jueves';
+    } else if (dia == 5) {
+      return 'Viernes';
+    } else if (dia == 6) {
+      return 'Sabado';
+    }
+    return 'Domingo';
   }
 }
--- lib/Pages/kardex_page.dart
+++ lib/Pages/kardex_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:sicedroid/Models/kardex.dart';
+import 'package:sicedroid/Models/materia_kardex.dart';
 import 'package:sicedroid/Routes/routes.dart';
 import 'package:sicedroid/Utils/singleton.dart';
+import 'package:sicedroid/Widgets/back_button_handler.dart';
 import 'package:sicedroid/Widgets/infinite_loading.dart';
 import 'package:sicedroid/Widgets/main_drawer.dart';
+import 'package:sicedroid/Utils/theme.dart' as theme;
 
 class KardexPage extends StatefulWidget {
   static const String routeName = '/kardex';
@@ -22,29 +25,89 @@ class _KardexPageState extends State<KardexPage> {
         context: context,
         page: Routes.kardex,
       ),
-      body: FutureBuilder(
-        future: Singleton.get().webServiceAlumnos.kardexConPromedio(),
-        builder: (buildContext, snapshot) {
-          if (snapshot.connectionState != ConnectionState.done) {
-            return InfiniteLoading();
-          } else if (!snapshot.hasData) {
-            return Center(child: Text('Conexión fallida, intnete más tarde'));
-          }
-          return _getBody(snapshot.data);
+      body: BackButtonHandler(context,
+          child: FutureBuilder(
+            future: Singleton.get().webServiceAlumnos.kardexConPromedio(),
+            builder: (buildContext, snapshot) {
+              if (snapshot.connectionState != ConnectionState.done) {
+                return InfiniteLoading();
+              } else if (!snapshot.hasData) {
+                return Center(
+                    child: Text('Conexión fallida, intnete más tarde'));
+              }
+              return SingleChildScrollView(child: _getTable(snapshot.data));
+            },
+          )),
+    );
+  }
+
+  Widget _getTable(Kardex kardex) {
+    var width = MediaQuery.of(context).size.width;
+    List<TableRow> tableRows = [];
+    tableRows.add(TableRow(children: <Widget>[
+      _headerColumn('Materia'),
+      _headerColumn('Calificación'),
+      _headerColumn('Acreditación')
+    ]));
+    kardex.materias.sort((m1, m2) => m1.clvMat.compareTo(m2.clvMat));
+    kardex.materias.forEach((m) => tableRows.add(_materiaColumn(materia: m)));
+    //Entre los width-s del margen y las columnas, el acumulado es 1
+    return Container(
+      margin: EdgeInsets.only(left: width * .01, right: width * .01, top: 10),
+      child: Table(
+        columnWidths: {
+          0: FixedColumnWidth(width * .48),
+          1: FixedColumnWidth(width * .25),
+          2: FixedColumnWidth(width * .25),
         },
+        children: tableRows,
+        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+        border: TableBorder.all(color: Colors.green[600]),
       ),
     );
   }
 
-  Widget _getBody(Kardex kardex) {
+  Widget _headerColumn(String text) {
     return Container(
-      color: Colors.cyan,
-      child: Column(
-        children: <Widget>[
-          Text(kardex.materias.toString()),
-          Text(kardex.promedio.toString())
-        ],
+        color: Colors.green,
+        child: Center(
+            child: Text(
+          text,
+          style: TextStyle(color: Colors.white),
+        )));
+  }
+
+  TableRow _materiaColumn({@required MateriaKardex materia}) {
+    var height = 50.0;
+    return TableRow(children: <Widget>[
+      Container(
+        height: height,
+        child: Center(
+            child: Text(
+          materia.materia + '\n' + materia.clvMat,
+          textAlign: TextAlign.center,
+        )),
       ),
-    );
+      Container(
+        height: height,
+        color: theme.getColorByCalif(materia.calif),
+        child: Center(
+          child: Text(
+            materia.calif.toString(),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      Container(
+        height: height,
+        child: Center(
+          child: Text(
+            materia.acred,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    ]);
   }
 }
